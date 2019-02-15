521bb7c6790f98b8e37bd80f7d9ba68a4780a8d2
Jonathan Cuevas <cuevasllaverias@gmail.com>
2019-02-15 18:23:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,10 +2,9 @@ var katzDeliLine = [];
 
 function takeanumber (currline, name) {
   currline.push(name)
-  
+
   for (var i = 0; i < array.length; i++) {
     array[i]
   }
 
 var takeanumber = [];
-
